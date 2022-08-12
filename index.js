6ddb13fdb8be7784424d3131443f95068c591b2a
alexis bustamante <alexisbustamantecisternas@gmail.com>
2022-08-12 16:58:07 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,100 @@
+//VARIABLES GLOBALES
+let cantCards = 6;//se raran 2 tarjetas por cada opcion
+let arrCards = [];
+let container = document.getElementById("ctn-main");
+let numOrden = 0
+let cantClick = 0;
+let arrSelected = [];
+
+//cuando el body este listo se ejecuta funcion MAIN
+document.body.onload = main();
+
+/***FUNCTIONS */
+function main() {
+    console.log('Starting')
+    createArrCards();
+    createCardDOM();
+    addEventToCards();
+}
+
+function handleClick(e) {
+    //
+    let id = e.path[2].id
+    let obj = arrCards.find((c) => c.id = id)
+
+    //buscar dentro del array si esta seleccionado que
+    let divInner = e.path[1];
+    cantClick++;
+    divInner.classList.toggle('is-flipped');
+    console.log(cantClick);
+}
+
+function addEventToCards() {
+    arrCards.map((card) => {
+        let div = document.getElementById(card.id);
+        div.addEventListener('click', handleClick);
+    })
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function createArrCards() {
+    let cardnum = 0;
+    let numImg = 1;
+    //esta funcion llenara el arrays de objetos con valores random
+    //-crear numero random para el orde
+    for (let index = 0; index < cantCards; index++) {
+        numImg = numImg == 4 ? 1 : numImg;
+        for (let c = 0; c < 2; c++) {
+            let numOrden = getRandomInt(1, 100);
+            let front = `/img/cardFront${numImg}.JPG`;
+            let obCard = {
+                id: `card${cardnum}`,
+                img_back: '/img/cardback.JPG',
+                img_front: front,
+                selected: false,
+                order: numOrden
+            }
+            arrCards.push(obCard);
+            cardnum++;
+        }
+        numImg++;
+    }
+
+    //se ordena de menor a mayor por numero aleatorios
+    arrCards.sort(((a, b) => a.order - b.order));
+}
+
+
+function createCardDOM() {
+
+    let htmlSring = '';
+    for (let index = 0; index < arrCards.length; index++) {
+        let html = `
+            <div class="card" id="${arrCards[index].id}">
+                <div class="card__inner" id="inner_${arrCards[index].id}">
+                    <div class="card__face card__face--front" style="background-image: url('/img/cardBack.JPG')">
+                    </div>
+                    <div class="card__face card__face--back" style="background-image: url('${arrCards[index].img_front}')">
+                    </div>
+                </div>
+            </div>`
+        htmlSring = htmlSring + html;
+    }
+    container.innerHTML = htmlSring;
+}
+
+// const card = document.querySelector('.card__inner');
+// let cantidadClicks = 0;
+
+// card.addEventListener('click', function () {
+//     console.log(cantidadClicks)
+//     cantidadClicks++
+//
+//     console.log(cantidadClicks);
+// }
+// )
